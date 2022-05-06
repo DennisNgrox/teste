de165c43e5414bf06deb0363010ec2e226ad454d
Dennis Silva <81188924+DennisNgrox@users.noreply.github.com>
2022-05-06 12:56:28 -0300

    Create trigger.sh

---
+++ trigger.sh
@@ -0,0 +1,71 @@
+
+#!/bin/bash
+
+ZABBIX_USER="Admin"
+ZABBIX_PASS="zabbix"
+ZABBIX_API="http://192.168.126.139/api_jsonrpc.php"
+
+ZABBIX_AUTH_TOKEN=$(curl -s -H  'Content-Type: application/json-rpc' -d "{\"jsonrpc\": \"2.0\",\"method\":\"user.login\",\"params\":{\"user\":\""${ZABBIX_USER}"\",\"password\":\""${ZABBIX_PASS}"\"},\"auth\": null,\"id\":0}" $ZABBIX_API |  jq -r .result)
+
+curl -s -H  'Content-Type: application/json-rpc' -d "
+
+{
+    \"jsonrpc\": \"2.0\",
+    \"method\": \"trigger.get\",
+    \"params\": {
+        \"triggerids\": \"20100\",
+        \"output\": \"extend\",
+        \"selectFunctions\": \"extend\"
+    },
+    \"auth\": \"${ZABBIX_AUTH_TOKEN}\",
+    \"id\": 1
+}" ${ZABBIX_API}
+
+in/bash
+
+ZABBIX_USER="Admin"
+ZABBIX_PASS="zabbix"
+ZABBIX_API="http://192.168.126.139/api_jsonrpc.php"
+
+ZABBIX_AUTH_TOKEN=$(curl -s -H  'Content-Type: application/json-rpc' -d "{\"jsonrpc\": \"2.0\",\"method\":\"user.login\",\"params\":{\"user\":\""${ZABBIX_USER}"\",\"password\":\""${ZABBIX_PASS}"\"},\"auth\": null,\"id\":0}" $ZABBIX_API |  jq -r .result)
+
+curl -s -H  'Content-Type: application/json-rpc' -d "
+
+{
+    \"jsonrpc\": \"2.0\",
+    \"method\": \"trigger.update\",
+    \"params\": {
+        \"triggerid\": \"20122\",
+       \"type\": \"0\"
+
+    },
+    \"auth\": \"${ZABBIX_AUTH_TOKEN}\",
+    \"id\": 1
+}" ${ZABBIX_API}
+
+
+
+-------------------------------------------------------------
+
+#!/bin/bash
+
+ZABBIX_USER="Admin"
+ZABBIX_PASS="zabbix"
+ZABBIX_API="http://192.168.126.139/api_jsonrpc.php"
+
+ZABBIX_AUTH_TOKEN=$(curl -s -H  'Content-Type: application/json-rpc' -d "{\"jsonrpc\": \"2.0\",\"method\":\"user.login\",\"params\":{\"user\":\""${ZABBIX_USER}"\",\"password\":\""${ZABBIX_PASS}"\"},\"auth\": null,\"id\":0}" $ZABBIX_API |  jq -r .result)
+
+curl -s -H  'Content-Type: application/json-rpc' -d "
+
+{
+    \"jsonrpc\": \"2.0\",
+    \"method\": \"template.get\",
+    \"params\": {
+        \"groupids\": \"10\",
+        
+    },
+    \"auth\": \"${ZABBIX_AUTH_TOKEN}\",
+    \"id\": 1
+}" ${ZABBIX_API}
+
+---------------------------------
